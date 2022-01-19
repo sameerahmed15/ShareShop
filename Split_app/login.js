99aa337a5aa97586470a4839a5c45884e2b8c03a
Sameer Ahmed <sameera@mun.ca>
2022-01-19 19:05:10 -0330

    logo added on the login page

--- Split_app/login.js
+++ Split_app/login.js
@@ -10,14 +10,32 @@ import React from 'react';
 import {
   View,
   Text,
+  Image,
+  StyleSheet,
 } from 'react-native';
 
 const Login = () => {
   return (
-    <Text>
-      HELLOOO WORLD
-    </Text>
+    <>
+      <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('C:/Users/samee/ShareShop/Split_app/assets/logo.png')}
+      />
+    </View>
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+});
 
 export default Login;
