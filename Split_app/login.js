f141766f23a92e45fbf471b4f98613f7fc7df35c
Sameer Ahmed <sameera@mun.ca>
2022-01-19 18:24:35 -0330

    clean setup hello world

---
+++ Split_app/login.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const Login = () => {
+  return (
+    <Text>
+      HELLOOO WORLD
+    </Text>
+  );
+};
+
+export default Login;
